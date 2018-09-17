19300e466e87d3ff1fad55e06a303085c5150885
jaime.de.los.rios <100315460@alumnos.uc3m.es>
2018-09-17 07:47:00 -0500

    Changes in the entropytriangle file

--- entropytriangle/entropytriangle.py
+++ entropytriangle/entropytriangle.py
@@ -49,35 +49,6 @@ def varnames (li):
     return dict((name,eval(name)) for name in li )
     
     
-
-def etplot (edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200 , gridl = 20 , ticks_size = 15 , pltscale = 20, chart_title = ""):
-    
-    """
-    Function for creating and showing the plots of the entropy triangle, independentlly of the type of triangle (SMET or CMET)
-
-    > etplot(edf, scale = x)
-
-    Parameters
-    ----------
-    edf : Dataframe with the entropic measures calculated
-    names : Used for plotting a list of Daframes (Defalut = NONE) 
-            In case of list: Names of the dataframes (if empty, some names will be provided for the plotting)
-    scale : Scale for the entropy triangle
-    fonts : Fontsize of the Labels
-    s_mk : Size of the marks for the triangle
-    gridl : Space between each gridline
-    tick_size: size of the numbers for the ticks
-    pltscale : Scale of the matplotlib window
-
-    Returns
-    ----------
-    points : painted points 
-    Shows the triangle
-    
-    """ 
-    if(not isinstance(edf,pd.DataFrame)):
-            exit("Can only work with Data Frames or lists of DataFrames! (df it´s not a DataFrame)")
-    
     
 def entriangle (edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200 
                      ,gridl = 20 , ticks_size = 15 , pltscale = 20, 
@@ -213,58 +184,6 @@ def entriangle (edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200
     
     return points
 
-
-
-    
-    
-def cmetplot(edf, names = None, scale = 100 , fonts = 30 ,s_mk = 200 , gridl = 20 , ticks_size = 15 , pltscale = 17, chart_title = ""):
-
-    if(not isinstance(edf, list)):
-        exit('This function only works with LISTS of Dataframes, maybe you can try the etplot function for an individual DataFrame plot')
-    
-    if(not all(isinstance(edf[x],pd.DataFrame) for x in range(len(edf)))):
-        exit('All values must be instances of DataFrames for entropy calculations')
-        
-    if(not names):
-        warning('No names founded, Providing Dummy names')
-        names = list(map(lambda x: "Features"+str(len(edf)-x)+"PC", range(len(edf))))
-             
-    
-    if(all(hasCmetEntropicCoords(edf[l]) for l in range(len(edf)))):
-
-        figure, tax = ternary_axes_subplot.figure(scale=scale)
-        figure.set_size_inches(pltscale, pltscale)
-        tax.boundary(linewidth=2.0)
-        tax.gridlines(multiple = gridl, color="blue")
-        
-        
-        colors = get_cmap(len(edf[0].index))
-        for i in range(len(edf)):
-            
-            points = entcoords(edf[i], scale)
-            mk = markers(1)*len(edf[i].index) ; names = list(edf[i].index+'-FT-'+str(len(edf)-i)+'-PC')
-            for j in range(len(edf[i].index)):
-                tax.scatter(points[j:j+1], s = s_mk , marker = mk[j], color = colors(j), label = names[j] ,edgecolor='black', linewidth='0.3')
-
-        if(chart_title == ""):
-            tax.set_title("Aggregate Channel Multivariate entropies (CMET)", fontsize = fonts + 5)
-        else:
-            tax.set_title(chart_title, fontsize = fonts + 5)
-            
-        tax.left_axis_label(r"$ VI'_{P_\overline{XY}}$", fontsize=fonts)
-        tax.right_axis_label(r"$ 2\cdot{I'_{P_\overline{XY}}}  $", fontsize=fonts)
-        tax.bottom_axis_label(r"$\Delta H'_{P_\overline{XY}}$", fontsize=fonts)
-
-    else: 
-        
-        exit("Entropic coordinates needed for the diagram plotting")
-
-    tax.ticks(axis='lbr', linewidth=1, multiple = gridl , fontsize = ticks_size )
-    tax.clear_matplotlib_ticks()
-    tax.legend(title = 'Features' ,labelspacing = 1.5 , fontsize = 12)
-    
-    tax.show()
-    
     
     
 def entriangle_list(edf, names = None, scale = 100 , fonts = 30 ,s_mk = 200 , gridl = 20 , ticks_size = 15 , pltscale = 17, chart_title = ""):
