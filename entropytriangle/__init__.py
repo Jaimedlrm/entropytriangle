69b0fb7d1dcc05ec1a5bec29305c03472caf3810
jaime.de.los.rios <100315460@alumnos.uc3m.es>
2018-09-20 01:13:18 -0500

    Upload of the v17 and the vignettes in pdf format

--- entropytriangle/__init__.py
+++ entropytriangle/__init__.py
@@ -3,4 +3,5 @@ from .auxfunc import (discretization, sjoin, condentropy)
 from .sentropies import *
 from .jentropies import *
 from .ternary import *
+from .coordsentropic import entcoords
 from .entropytriangle import entriangle, entriangle_list
--- entropytriangle/entropytriangle.py
+++ entropytriangle/entropytriangle.py
@@ -5,7 +5,10 @@ Module for plotting the ET
 '''
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 
+from matplotlib.lines import Line2D
 from sys import exit as exit
 from .ternary import ternary_axes_subplot
 from .coordsentropic import *
@@ -13,7 +16,7 @@ from random import choice
 
 #Auxiliar functions
 
-def get_cmap(number):
+def get_cmap(number, cmet = "1"):
 
     """
     Used for generating a set of random colors in order to differenciate the coordinates of each variable in the diagram. 
@@ -30,9 +33,12 @@ def get_cmap(number):
     colors : Returns a set of colors for creating the scatter plot (matplotlib.colors.LinearSegmentedColormap)
 
     """
-    #cmaps = list(['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds','YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu','GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn'])
-    cmaps = list(['spring','summer','autumn','winter','cool','Wistia'])
-    cmaps = list(['Wistia'])
+    if(cmet=="1"):
+        #cmaps = list(['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds','YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu','GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn'])
+        cmaps = list(['spring','summer','autumn','winter','cool','Wistia'])
+        cmaps = list(['summer'])
+    else:
+        cmaps = list(['summer'])
     return plt.cm.get_cmap(choice(cmaps) , number)
 
 
@@ -40,14 +46,10 @@ def get_cmap(number):
 def markers(n) :
     
     #filled_markers = ('o', '*','+', 'x','D') ; mk = list()
-    #filled_markers = ('o','D','h') ; mk = list()
     filled_markers = ('o') ; mk = list()
     for i in range(n) : mk.append(choice(filled_markers))
     return mk
 
-def varnames (li):
-    return dict((name,eval(name)) for name in li )
-    
     
     
 def entriangle (edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200 
@@ -99,7 +101,7 @@ def entriangle (edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200
         mk = markers(1)*len(edf)
         
         for i in range(len(edf.index)):
-            tax.scatter(points[i:i+1], s = s_mk, marker = mk[i], color = colors(i), label = names[i] ,edgecolor='black', linewidth='0.5')
+            tax.scatter(points[i:i+1], s = s_mk, marker = mk[i], color = colors(i), label = names[i] ,edgecolor='black', linewidth='0.1')
 
         if (hasSplitSmetCoords(edf)):
 
@@ -116,7 +118,7 @@ def entriangle (edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200
             if(chart_title != ""):
                 tax.set_title(chart_title, fontsize = fonts + 5)
             else:
-                tax.set_title("Aggregate Source Multivariate entropies (SMET)", fontsize = fonts + 5)
+                tax.set_title("Source Multivariate entropies (SMET)", fontsize = fonts + 5)
             tax.left_axis_label(r"$\Delta H'_{Pi_{X}}$", fontsize=fonts)
             tax.right_axis_label(r"$ M'_{P_{X}}$", fontsize=fonts)
             tax.bottom_axis_label(r"$ VI'_{P_{X}}$", fontsize=fonts)
@@ -126,7 +128,7 @@ def entriangle (edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200
             if(chart_title != ""):
                 tax.set_title(chart_title, fontsize = fonts + 5)
             else:
-                tax.set_title("Dual Aggregate Source Multivariate entropies (SMET)", fontsize = fonts + 5)
+                tax.set_title("Dual Source Multivariate entropies (SMET)", fontsize = fonts + 5)
             tax.left_axis_label(r"$\Delta H'_{P_{X}}$", fontsize=fonts)
             tax.right_axis_label(r"$ D'_{P_{X}}$", fontsize=fonts)
             tax.bottom_axis_label(r"$ VI'_{P_{X}}$", fontsize=fonts)
@@ -181,27 +183,54 @@ def entriangle (edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200
     tax.legend(title = 'Features' ,labelspacing = 1.5 , fontsize = 12)
     
     tax.show()
+    figure.savefig('plot.pdf')
     
-    return points
+    #return tax
 
     
     
 def entriangle_list(edf, names = None, scale = 100 , fonts = 30 ,s_mk = 200 , gridl = 20 , ticks_size = 15 , pltscale = 17, chart_title = ""):
     
-    '''
-    You can only pass list of aggregated entropies values.
-    '''
+    """
+    Function for creating and plotting points of the entropy triangle, independentlly of the type of triangle (CBET, SMET or CMET)
+
+    > entropytriangle(edf,names = None, scale = 100 , fonts = 30 ,s_mk = 200 
+                     ,gridl = 20 , ticks_size = 15 , pltscale = 20, 
+                     chart_title = ""):
+
+    Parameters
+    ----------
+    edf : Dataframe with the entropic measures calculated
+    names : Used for plotting a list of Daframes (Defalut = NONE) 
+            In case of list: Names of the dataframes (if empty, some names will be provided for the plotting)
+    scale : Scale for the entropy triangle
+    fonts : Fontsize of the Labels
+    s_mk : Size of the marks for the triangle
+    gridl : Space between each gridline
+    tick_size: size of the numbers for the ticks
+    pltscale : Scale of the matplotlib window
+    chart_title: Plot´s title
+    
+
+    Returns
+    ----------
+    points : painted points 
+    Shows the triangle
+    
+    """
 
     if(not isinstance(edf, list)):
         exit('This function only works with LISTS of Dataframes, maybe you can try the etplot function for an individual DataFrame plot')
     
     if(not all(isinstance(edf[x],pd.DataFrame) for x in range(len(edf)))):
         exit('All values must be instances of DataFrames for entropy calculations')
-        
+    
+    '''   
     if(not names):
         warning('No names founded, Providing Dummy names')
         names = list(map(lambda x: "Knn"+str(len(edf)-x)+"PC", range(len(edf))))
-     
+    '''
+    
     if(all(hasCbetEntropicCoords(edf[l]) for l in range(len(edf)))):
         
         figure, tax = ternary_axes_subplot.figure(scale=scale)
@@ -232,33 +261,117 @@ def entriangle_list(edf, names = None, scale = 100 , fonts = 30 ,s_mk = 200 , gr
         figure.set_size_inches(pltscale, pltscale)
         tax.boundary(linewidth=2.0)
         tax.gridlines(multiple = gridl, color="blue")
-        
-        marker = list(['o','h','D', 's','d','v','^'])
-        #marker = list(['v','^', '<','>'])
-        colors = get_cmap(len(edf[0].index))
-        
-        for i in range(len(edf)): 
-            points = entcoords(edf[i], scale)
-            mk = marker[i]*len(edf[i].index) ; names = list(edf[i].index+'-FT-'+str(len(edf)-i)+'-PC')
-            
-            for j in range(len(edf[i].index)):
-                tax.scatter(points[j:j+1], s = s_mk , marker = mk[j], color = colors(j), label = names[j] ,edgecolor='black', linewidth='0.4')
 
+        marker = list(['X','v','o'])
+        colors = get_cmap(len(edf))
+
+        for i in range(3): 
+            for j in range(len(edf)):
+                points = entcoords(edf[j].take([i]), scale)
+                mk = marker[i]*len(edf) ; names = str(edf[i].index[i]+' with '+str(len(edf)-j)+' PC')
+
+                if(i != 1):
+                    tax.scatter(points, s = s_mk , marker = mk[j], color=colors(j), linewidth='0.1')
+                
+                else:
+                    tax.scatter(points, s = s_mk , marker = mk[j], color ='white' , edgecolors = colors(j), linewidth='1.5')
+                
         if(chart_title == ""):
-            tax.set_title("Aggregate Channel Multivariate entropies (CMET)", fontsize = fonts + 5)
+            tax.set_title("Channel Multivariate entropies (CMET)", fontsize = fonts + 5)
         else:
             tax.set_title(chart_title, fontsize = fonts + 5)
-            
+
+        tax.left_axis_label(r"$ VI'_{P_\overline{XY}}$", fontsize=fonts)
+        tax.right_axis_label(r"$ 2\cdot{I'_{P_\overline{XY}}}  $", fontsize=fonts)
+        tax.bottom_axis_label(r"$\Delta H'_{P_\overline{XY}}$", fontsize=fonts)
+       
+        leg = list()
+        leg.append(mpatches.Patch(color='white', label='Feature Type'))
+        
+        for i in range(len(edf)):leg.append(mpatches.Patch(color=colors(i), label=str(len(edf)-i)+' Principal Components'))
+        leg.append(mpatches.Patch(color='white', label=''))
+        leg.append(mpatches.Patch(color='white', label='Entropy types'))
+    
+        leg.append(mlines.Line2D([], [], color='grey', marker='X', linestyle='None',markersize=15, label='X')); leg.append(mlines.Line2D([], [], color='grey', marker='v', linestyle='None',markersize=15, label='Y')); leg.append(mlines.Line2D([], [], color='grey', marker='o', linestyle='None',markersize=15, label='XY'))
+
+
+    tax.ticks(axis='lbr', linewidth=1, multiple = gridl , fontsize = ticks_size )
+    tax.clear_matplotlib_ticks()
+    
+    if(all(hasCmetEntropicCoords(edf[l]) for l in range(len(edf)))):
+        
+        tax.legend(handles=leg, loc = 1 ,labelspacing = 1 , fontsize = 14)
+    else:
+        tax.legend(title = 'Features' ,labelspacing = 1.5 , fontsize = 12)
+    
+    
+    tax.show()
+    figure.savefig('plot.pdf')
+    #return tax
+    
+    
+    
+    
+    
+    
+    
+
+'''
+def cmetplot(edf, names = None, scale = 100 , fonts = 30 ,s_mk = 200 , gridl = 20 , ticks_size = 15 , pltscale = 17, chart_title = ""):
+    
+
+    if(all(hasCmetEntropicCoords(edf[l]) for l in range(len(edf)))):
+
+        figure, tax = ternary_axes_subplot.figure(scale=scale)
+        figure.set_size_inches(pltscale, pltscale)
+        tax.boundary(linewidth=2.0)
+        tax.gridlines(multiple = gridl, color="blue")
+
+        marker = list(['X','v','o'])
+        colors = get_cmap(len(edf))
+
+        for i in range(3): 
+            for j in range(len(edf)):
+                points = entcoords(edf[j].take([i]), scale)
+                mk = marker[i]*len(edf) ; names = str(edf[i].index[i]+' with '+str(len(edf)-j)+' PC')
+
+                if(i != 1):
+                    tax.scatter(points, s = s_mk , marker = mk[j], label = names, color=colors(j), linewidth='0.1')
+                
+                else:
+                    tax.scatter(points, s = s_mk , marker = mk[j], label = names, color ='white' , edgecolors = colors(j), linewidth='1.5')
+                
+        if(chart_title == ""):
+            tax.set_title("Channel Multivariate entropies (CMET)", fontsize = fonts + 5)
+        else:
+            tax.set_title(chart_title, fontsize = fonts + 5)
+
         tax.left_axis_label(r"$ VI'_{P_\overline{XY}}$", fontsize=fonts)
         tax.right_axis_label(r"$ 2\cdot{I'_{P_\overline{XY}}}  $", fontsize=fonts)
         tax.bottom_axis_label(r"$\Delta H'_{P_\overline{XY}}$", fontsize=fonts)
 
 
+    leg = list()
+    leg.append(mpatches.Patch(color='white', label='Feature Type'))
+    for i in range(len(edf)):leg.append(mpatches.Patch(color=colors(i), label=str(len(edf)-i)+' Principal Components'))
+    leg.append(mpatches.Patch(color='white', label=''))
+    leg.append(mpatches.Patch(color='white', label='Entropy types'))
+    
+    leg.append(mlines.Line2D([], [], color='grey', marker='X', linestyle='None',markersize=15, label='X')); leg.append(mlines.Line2D([], [], color='grey', marker='v', linestyle='None',markersize=15, label='Y')); leg.append(mlines.Line2D([], [], color='grey', marker='o', linestyle='None',markersize=15, label='XY'))
+        
     tax.ticks(axis='lbr', linewidth=1, multiple = gridl , fontsize = ticks_size )
     tax.clear_matplotlib_ticks()
-    tax.legend(title = 'Features' ,labelspacing = 1.5 , fontsize = 12)
+    #tax.legend(title = 'Features' ,labelspacing = 1.5 , fontsize = 12)
     
+
+    
+
+    tax.legend(handles=leg, loc = 1 ,labelspacing = 1 , fontsize = 14)
+
+
+            
     tax.show()
+    figure.savefig('plot.pdf')
+    #return tax
 
-
-
+'''
